7671c8891ab5305dc931e984a686239e609b6a2c
Sylvio Neto <dmsylvio@gmail.com>
2020-07-19 19:00:39 -0400

    com guepard

--- map/skill.cpp
+++ map/skill.cpp
@@ -12731,6 +12731,13 @@ static int skill_dance_overlap_sub(struct block_list* bl, va_list ap)
 //When 1, this unit has been positioned, so start the cancel effect.
 int skill_dance_overlap(struct skill_unit* unit, int flag)
 {
+
+// (^~_~^) LGP Start
+
+	return 0;
+
+// (^~_~^) LGP End
+
 	if (!unit || !unit->group || !(unit->group->state.song_dance&0x1))
 		return 0;
 	if (!flag && !(unit->val2&UF_ENSEMBLE))
@@ -19074,6 +19081,11 @@ void skill_unit_move_unit_group(struct skill_unit_group *group, int16 m, int16 d
 		switch(m_flag[i]) {
 			case 0:
 			//Cell moves independently, safely move it.
+
+				// (^~_~^) LGP Start
+				clif_skill_delunit(unit1);
+				// (^~_~^) LGP End
+
 				map_foreachinmovearea(clif_outsight, &unit1->bl, AREA_SIZE, dx, dy, BL_PC, &unit1->bl);
 				map_moveblock(&unit1->bl, unit1->bl.x+dx, unit1->bl.y+dy, tick);
 				break;
@@ -19085,6 +19097,11 @@ void skill_unit_move_unit_group(struct skill_unit_group *group, int16 m, int16 d
 					if(m_flag[j] != 2 || !group->unit[j].alive)
 						continue;
 					//Move to where this cell would had moved.
+
+					// (^~_~^) LGP Start
+					clif_skill_delunit(unit1);
+					// (^~_~^) LGP End
+
 					unit2 = &group->unit[j];
 					dx2 = unit2->bl.x + dx - unit1->bl.x;
 					dy2 = unit2->bl.y + dy - unit1->bl.y;
